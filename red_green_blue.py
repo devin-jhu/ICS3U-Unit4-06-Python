c3afe287020bc770b1f9a926888d49463ad912f9
Devin Jhu <devin.jhu@mths.ca>
2022-05-14 02:41:35 +0000

    code

--- red_green_blue.py
+++ red_green_blue.py
@@ -12,9 +12,9 @@ def main():
     blue = 0
 
     # process & output
-    for red in range(255):
-        for green in range(255):
-            for blue in range(255):
+    for red in range(256):
+        for green in range(256):
+            for blue in range(256):
                 print("RGB {0}, {1}, {2}".format(red, green, blue))
 
     print("\nDone.")
